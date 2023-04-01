d641f8842c95f457f9fa6a4a8c449fb6cc44ad34
SGTMAX0723 <97983910+SGTMAX0723@users.noreply.github.com>
2023-04-01 18:43:57 -0500

    Maximus Rodriguez, worked on unit testing for conversion API

--- test/bitcoin_conversion_api_test.dart
+++ test/bitcoin_conversion_api_test.dart
@@ -6,23 +6,31 @@ import 'package:http/http.dart' as http;
 class MockClient extends Mock implements http.Client {}
 
 main() {
-  test('our fetchConversion call returns our rate converion correctly',
-      () async {
-    final client = MockClient();
-    final fakeConversionRateAPIData =
-        '{"time":{"updated":"Apr 1, 2023 22:46:00 UTC","updatedISO":"2023-04-01T22:46:00+00:00","updateduk":"Apr 1, 2023 at 23:46 BST"},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org","bpi":{"USD":{"code":"USD","rate":"28,540.3880","description":"United States Dollar","rate_float":28540.388}}}';
-    var url =
-        Uri.parse('https://api.coindesk.com/v1/bpi/currentprice/usd.json');
+  group('fetchConversion', () {
+    test('returns a String if the http call completes successfully', () async {
+      final client = MockClient();
+      final fakeConversionAPIData =
+          '{"time":{"updated":"Apr 1, 2023 22:52:00 UTC","updatedISO":"2023-04-01T22:52:00+00:00","updateduk":"Apr 1, 2023 at 23:52 BST"},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org","bpi":{"USD":{"code":"USD","rate":"28,536.0012","description":"United States Dollar","rate_float":28536.0012}}}';
 
-    when(client.get(url))
-        .thenAnswer((_) async => http.Response(fakeConversionRateAPIData, 200));
+      var url =
+          Uri.parse('https://api.coindesk.com/v1/bpi/currentprice/usd.json');
 
-    String conversion = await CalculationTools.fetchConversion(http.Client());
+      when(client.get(url))
+          .thenAnswer((_) async => http.Response(fakeConversionAPIData, 200));
 
-    expect(conversion, isA<String>());
-    expect(conversion, "28,540.3880");
+      String conversion = await CalculationTools.fetchConversion(client);
+      expect(conversion, isA<String>());
+      expect(conversion, "28,536.0012");
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+      var url =
+          Uri.parse('https://api.coindesk.com/v1/bpi/currentprice/usd.json');
+      when(client.get(url))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(CalculationTools.fetchConversion(client), throwsException);
+    });
   });
-
-  test('our fetchConversion call returns our rate converion incorrectly',
-      () async {});
 }
